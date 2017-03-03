0ffaee707b92be47493c4cc5901357d61b59526f
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-04 04:11:14 +0700

    Rollback because don need that quantity

--- db/migrate/20170303160415_add_sold_to_ticket_types.rb
+++
@@ -1,5 +0,0 @@
-class AddSoldToTicketTypes < ActiveRecord::Migration[5.0]
-  def change
-    add_column :ticket_types, :sold_quantity, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303160415) do
+ActiveRecord::Schema.define(version: 20151215104433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,8 @@ ActiveRecord::Schema.define(version: 20170303160415) do
     t.integer  "price"
     t.string   "name"
     t.integer  "max_quantity"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "sold_quantity"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.index ["event_id"], name: "index_ticket_types_on_event_id", using: :btree
   end
 
