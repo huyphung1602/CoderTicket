bd94b554abccc9d00e874b925a509feadae13760
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-07 16:25:45 +0700

    Fixing bug with js when login with facebook

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,3 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-
-$( document ).ready(function() {
-    var heights = $(".card").map(function() {
-        return $(this).height();
-    }).get(),
-
-    maxHeight = Math.max.apply(null, heights);
-
-    $(".card").height(maxHeight);
-});
