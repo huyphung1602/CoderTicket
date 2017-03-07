c657c3810f063a944ec5fb60e57c20e00ac68616
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-07 15:28:31 +0700

    Use js to adjust the height of the cards that have different long of content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,13 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$( document ).ready(function() {
+    var heights = $(".card").map(function() {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(".card").height(maxHeight);
+});
