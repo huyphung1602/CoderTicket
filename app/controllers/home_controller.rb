ff1ab5fedb2a29b97a67e451e871e460c116c91e
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-08 23:46:10 +0700

    Fix the visit as guest button 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
     if signed_in?
       redirect_to upcoming_path
     end
+
+    if params[:is_guest]
+      redirect_to upcoming_path
+    end
   end
 end
